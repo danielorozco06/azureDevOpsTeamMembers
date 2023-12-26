4e08e335a15f6b9e4a0eea2b0e69c44ccc2c232a
daorozc <daorozc@bancolombia.com.co>
2023-12-26 12:47:31 -0500

    Refactor Azure DevOps API authentication

--- wiki.py
+++ wiki.py
@@ -9,6 +9,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def get_azure_headers() -> dict[str, str]:
+    """Generate the authorization headers for Azure DevOps API."""
+    pat = os.getenv("AZURE_PAT")
+    encoded_pat = base64.b64encode(bytes(f":{pat}", "ascii")).decode("ascii")
+    return {
+        "Accept": "application/json",
+        "Authorization": f"Basic {encoded_pat}",
+    }
+
+
 def list_wikis(
     org_url: str, project: str, headers: dict[str, str]
 ) -> list[dict[str, str]]:
@@ -19,7 +29,7 @@ def list_wikis(
     return response.json()["value"]
 
 
-def get_wikis(org_url: str, project: str, headers: dict[str, str]) -> dict[str, str]:
+def get_wiki(org_url: str, project: str, headers: dict[str, str]) -> dict[str, str]:
     """Get azure wiki"""
     wiki_id = "10978e98-31dc-4fe7-9a40-7854c9591ef1"
     url = f"{org_url}/{project}/_apis/wiki/wikis/{wiki_id}?api-version=7.2-preview.2"
@@ -73,17 +83,11 @@ def remove_file(path_file: str) -> None:
 if __name__ == "__main__":
     org_url = os.getenv("AZURE_ORGANIZATION_URL")
     project_name = os.getenv("AZURE_PROJECT_NAME")
-    pat = os.getenv("AZURE_PAT")
-
-    authorization = str(base64.b64encode(bytes(f":{pat}", "ascii")), "ascii")
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Basic " + authorization,
-    }
+    headers = get_azure_headers()
 
     try:
         wikis = list_wikis(org_url, project_name, headers)
-        wiki = get_wikis(org_url, project_name, headers)
+        wiki = get_wiki(org_url, project_name, headers)
         get_pages_batch(org_url, project_name, headers)
         print("Finish")
     except Exception as error:
